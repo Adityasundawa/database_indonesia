96165f751f2b4b9128b77c2e1f8d8b2450cb7bfe
Aditya Sundawa <adityasundawa.co@gmail.com>
2020-04-11 12:31:37 +0700

    Database Indonesia

---
+++ provinsi.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2020 at 07:19 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `swarakalibata_marketplace`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rb_provinsi`
+--
+
+CREATE TABLE `rb_provinsi` (
+  `provinsi_id` int(11) NOT NULL,
+  `nama_provinsi` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rb_provinsi`
+--
+
+INSERT INTO `rb_provinsi` (`provinsi_id`, `nama_provinsi`) VALUES
+(1, 'Bali'),
+(2, 'Bangka Belitung'),
+(3, 'Banten'),
+(4, 'Bengkulu'),
+(5, 'DI Yogyakarta'),
+(6, 'DKI Jakarta'),
+(7, 'Gorontalo'),
+(8, 'Jambi'),
+(9, 'Jawa Barat'),
+(10, 'Jawa Tengah'),
+(11, 'Jawa Timur'),
+(12, 'Kalimantan Barat'),
+(13, 'Kalimantan Selatan'),
+(14, 'Kalimantan Tengah'),
+(15, 'Kalimantan Timur'),
+(16, 'Kalimantan Utara'),
+(17, 'Kepulauan Riau'),
+(18, 'Lampung'),
+(19, 'Maluku'),
+(20, 'Maluku Utara'),
+(21, 'Nanggroe Aceh Darussalam (NAD)'),
+(22, 'Nusa Tenggara Barat (NTB)'),
+(23, 'Nusa Tenggara Timur (NTT)'),
+(24, 'Papua'),
+(25, 'Papua Barat'),
+(26, 'Riau'),
+(27, 'Sulawesi Barat'),
+(28, 'Sulawesi Selatan'),
+(29, 'Sulawesi Tengah'),
+(30, 'Sulawesi Tenggara'),
+(31, 'Sulawesi Utara'),
+(32, 'Sumatera Barat'),
+(33, 'Sumatera Selatan'),
+(34, 'Sumatera Utara');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `rb_provinsi`
+--
+ALTER TABLE `rb_provinsi`
+  ADD PRIMARY KEY (`provinsi_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `rb_provinsi`
+--
+ALTER TABLE `rb_provinsi`
+  MODIFY `provinsi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
